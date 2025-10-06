8f10228b8acb89eef13c8de69be4138a6b806ec3
Yuri Gabriel Constantino <yuri.gconstantino@gmail.com>
2025-10-06 19:39:39 -0300

    feat: criado arquivos dedicados para amazenar os projetos e skills

---
+++ src/data/projects.ts
@@ -0,0 +1,44 @@
+const projects = [
+  {
+    image: '../../../public/image/Efood.png',
+    title: 'EFood',
+    description: 'Desenvolvimento de uma página de delivery de restaurantes.',
+    tag: ['React', 'Redux', 'TypeScript', 'Route-DOM'],
+    demo: 'https://efood-topaz-omega.vercel.app/',
+    repositorie: 'https://github.com/YuriGConstantino/efood',
+    delay: 0.1
+  },
+  {
+    image: '../../../public/image/Contacts-List.png',
+    title: 'Contacts List',
+    description:
+      'Desenvolvimento de uma lista de contatos feita em React e TypeScript.',
+    tag: ['React', 'Redux', 'TypeScript', 'Route-DOM'],
+    demo: 'https://contacts-list-livid.vercel.app/',
+    repositorie: 'https://github.com/YuriGConstantino/Contacts-List',
+    delay: 0.2
+  },
+  {
+    image: '../../../public/image/Clone-Crunchyroll.png',
+    title: 'Clone Crunchyroll',
+    description:
+      'Desenvolvimento de uma landing page clone de um site de streaming.',
+    tag: ['HTML', 'SASS', 'JavaScript', 'Gulp'],
+    demo: 'https://curso-ebac-frontend-projeto4.vercel.app/',
+    repositorie: 'https://github.com/YuriGConstantino/Clone-Crunchyroll',
+    delay: 0.3
+  },
+  {
+    image: '../../../public/image/Landing-Page-com-Bootstrap.png',
+    title: 'Landing Page com Bootstrap',
+    description:
+      'Desenvolvimento de uma landing page responsiva para um site fictício de venda de jogos.',
+    tag: ['HTML', 'Bootstrap', 'JavaScript'],
+    demo: 'https://curso-ebac-frontend-projeto3.vercel.app/',
+    repositorie:
+      'https://github.com/YuriGConstantino/Landing-Page-com-Bootstrap',
+    delay: 0.4
+  }
+]
+
+export default projects
---
+++ src/data/skills.ts
@@ -0,0 +1,48 @@
+
+const skills = [
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg',
+    skill: 'HTML5',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg',
+    skill: 'CSS3',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg',
+    skill: 'Bootstrap',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg',
+    skill: 'Sass',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
+    skill: 'JavaScript',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg',
+    skill: 'jQuery',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gulp/gulp-plain.svg',
+    skill: 'Gulp',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/grunt/grunt-original.svg',
+    skill: 'Grunt',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg',
+    skill: 'TypeScript',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+    skill: 'React',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg',
+    skill: 'Redux',
+  }
+]
+export default skills
