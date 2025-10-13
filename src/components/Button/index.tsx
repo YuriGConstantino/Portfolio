41529a25973f97616c41b219a11053b7332e58ec
Yuri Gabriel Constantino <yuri.gconstantino@gmail.com>
2025-10-13 20:33:02 -0300

    feat: add novo type no componete Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 type Props = {
-  type: 'button' | 'submit' | 'link'
+  type: 'button' | 'submit' | 'link' | 'download'
   className: string
   href?: string
   target?: string
   rel?: string
   children: string | React.ReactNode
   onClick?: () => void
+  download?: boolean
 }
 
 export const Button = ({
@@ -17,18 +18,21 @@ export const Button = ({
   target,
   rel,
   className,
-  children
+  children,
+  download
 }: Props) => {
   if (type === 'button' || type === 'submit') {
     return (
-      <button type={type} onClick={onClick} className={className}>
-        {children}
-      </button>
+      <>
+        <button type={type} onClick={onClick} className={className}>
+          {children}
+        </button>
+      </>
     )
   }
 
   return (
-    <a href={href} target={target} rel={rel} className={className}>
+    <a href={href} target={target} rel={rel} className={className} download={download}>
       {children}
     </a>
   )
